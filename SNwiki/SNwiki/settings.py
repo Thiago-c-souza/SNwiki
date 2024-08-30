340056487da39ef1042f9bd65746df2290860dbd
Thiago <thiagocaixetadesouza10@gmail.com>
2024-08-30 04:42:21 -0300

    feito o esquema de post

--- SNwiki/SNwiki/settings.py
+++ SNwiki/SNwiki/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
+    'ckeditor',
+    'ckeditor_uploader'
 ]
 
 MIDDLEWARE = [
@@ -116,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+CKEDITOR_UPLOAD_PATH = 'uploads/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- SNwiki/app/models.py
+++ SNwiki/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 # Create your models here.
+
+class rotinas(models.Model):
+    N_rotina = models.CharField(max_length=6)
+    Nome_rotina = models.CharField(max_length=50)
+
+class Post(models.Model):
+    Titulo = models.CharField(max_length=255)
+    rotina = models.ForeignKey(rotinas, on_delete=models.PROTECT)
+    Resumo = RichTextField()
+    
